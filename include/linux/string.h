be7968711ecc3d5e9a75e7f01f33f2b267dd580f
lasek0 <gigawaty@gmail.com>
2021-03-08 22:42:33 +0100

    fix strings save & restore registers | more to be done :(

--- include/linux/string.h
+++ include/linux/string.h
@@ -262,11 +262,13 @@ return __res;
 static inline size_t strlen(const char * s)
 {
 register int __res __asm__("cx");
-__asm__("cld\n\t"
+__asm__("push %%edi; push %%eax\n\t"
+        "cld\n\t"
 	"repne\n\t"
 	"scasb\n\t"
 	"notl %0\n\t"
-	"decl %0"
+	"decl %0\n\t"
+        "pop %%eax ; pop %%edi"
 	:"=c" (__res):"D" (s),"a" (0),"0" (0xffffffff):);
 return __res;
 }
